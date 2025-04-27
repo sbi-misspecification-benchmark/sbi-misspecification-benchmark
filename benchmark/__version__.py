17ee1a8893cfbe22b59177a26833bdc519bdf00a
Sinan Büyük <sinanalib@gmail.com>
2025-04-27 23:05:09 +0200

    Changed structure according to PO

--- benchmark/__version__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
-# under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
-
-VERSION = (0, 0, 0)
-
-__version__ = ".".join(map(str, VERSION))
