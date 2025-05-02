e970862535691498111b6ddcdc60d5072890c5c4
Daniel Wosylus <daniel.wosylus@student.uni-tuebingen.de>
2025-05-02 20:16:24 +0200

    updated comments

--- benchmark/run.py
+++ benchmark/run.py
@@ -12,7 +12,7 @@ def main():
     parser.add_argument("config", type=str, help="Path to the YAML configuration file.")
     args = parser.parse_args()
 
-    # Load configuration
+    # Load config
     config = load_config(args.config)
 
     # Handle random seed
@@ -27,7 +27,7 @@ def main():
     task = config.get('task')
     if task is not None:
         task_name = task.get('name', "No task name provided")
-        # Get arguments for testing purposes with defaults:
+        # Get keys for testing purposes w/o defaults:
         num_simulations = task.get('num_simulations')
         num_observations = task.get('num_observations')
         num_posterior_samples = task.get('num_posterior_samples')
@@ -37,13 +37,13 @@ def main():
 
     print(
         f"Executing task: {task_name}\n"
+        # printing keys for testing purposes
         f"num_simulations: {num_simulations}\n"
         f"num_observations: {num_observations}\n"
         f"num_posterior_samples: {num_posterior_samples}\n"
     )
 
     # Add logic to handle task execution => num_simulations, num_observations, num_posterior_samples
-
 
 if __name__ == "__main__":
     main()
