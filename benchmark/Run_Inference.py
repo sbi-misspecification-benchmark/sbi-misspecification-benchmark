45d5ae89049648b55073d6ab3d624d3bfdd9f692
Anmaka99 <a.m.kamer@web.de>
2025-05-15 12:41:43 +0200

    Update Run_Inference.py

--- benchmark/Run_Inference.py
+++ benchmark/Run_Inference.py
@@ -12,7 +12,9 @@ methods = {
     "NRE": NRE,
 }
 
-def run_inference(task, method_name, num_simulations):
+def run_inference(task, method_name, num_simulations, seed = None):
+    """Run simulation-based inference on a given task using the specified method.
+    Returns samples from the posterior conditioned on the true observation"""
     if method_name not in methods:
         raise ValueError(f"Method {method_name} is not supported. Choose from {list(methods.keys())}.")
     
@@ -21,18 +23,18 @@ def run_inference(task, method_name, num_simulations):
     prior = task.get_prior()
     simulator = task.get_simulator()
 
-## For testing purposes, we can use a fixed seed
-#   torch.manual_seed(0)
+    if seed is not None:
+        torch.manual_seed(seed)
  
 # draw parameters from prior
-    θ = prior.sample((num_simulations,))
+    theta = prior.sample((num_simulations,))
 
 # simulate data
-    x = simulator(θ)
+    x = simulator(theta)
 
 # create and train inference model
     inference = method_class(prior)
-    density_estimator = inference.append_simulations(θ, x).train()
+    density_estimator = inference.append_simulations(theta, x).train()
 
 # perform inference
     posterior = inference.build_posterior(density_estimator)
